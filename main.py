cca4ddca4511966d5889150eeb3093ec2ba927e7
Evgeny <e.tarlo@g.nsu.ru>
2023-04-19 14:13:30 +0700

    add if

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ Tarlo Evgeny
 Karpenko Nikolay
 """
 
+import ru_local as ru
+yes = ru.ans_yes
+no = ru.ans_no
+
+print(ru.q0)
+if str.lower(input()) == yes:  # tax resident
+    None
+else:  # non-tax resident
+    print(ru.q00)
+    if str.lower(input()) == yes:  # have income
+
+    else:  # don't have income
